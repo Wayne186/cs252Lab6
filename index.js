1d3ff17d106cb1966e787f1172cea0332f2952c8
Wayne186 <wayne186645@gmail.com>
2017-12-04 12:04:44 -0500

    Demo

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+var path = require('path')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', path.join(__dirname, 'views'))
+app.set('public', path.join(__dirname, 'js'))
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'js')))
+app.use(express.static(path.join(__dirname, 'views')))
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
