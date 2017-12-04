8a0b3c425b19ef5e89c58adfeda4464df0a764aa
Wayne186 <wayne186645@gmail.com>
2017-12-04 14:20:23 -0500

    Demo

--- views/js/load.js
+++ views/js/load.js
@@ -1,7 +1,8 @@
-var start = "Welcome to the world of "
+var start = "Welcome to the world of Grimgar"
 
 
 window.onload = function() {
 	html = '<p>'+start+'</p></br>'
 	$('.start').append(html);
 }
+
